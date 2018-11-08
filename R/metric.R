997bb3b7a24ccf286f2ed3d3f9d5efc3ea16f31a
larryleihua <larry.lei.hua@gmail.com>
2018-11-08 11:38:45 -0600

    acc_lucky

--- R/metric.R
+++ R/metric.R
@@ -6,29 +6,22 @@ acc_lucky <- function(train_class, test_class, my_acc, s=1000)
   nTrain <- sum(train_class)
   nTest <- sum(test_class)
 
-  if(nTrain_class!=nTest_class)
-    stop("Error: The number of classes in test and train sets are different!")
-  true_class <- unlist(sapply(seq_len(nTrain_class),
-                              function(i){rep(i, test_class[i])}))
+  if(nTrain_class!=nTest_class) stop("Error: The number of classes in test and train sets are different!")
+  true_class <- unlist(sapply(seq_len(nTrain_class), function(i){rep(i, test_class[i])}))
 
   random_guess <- sample(1:nTrain_class, nTest*s, replace = T)
   random_guess <- matrix(random_guess, s, nTest)
 
-  educated_guess <- sample(1:nTrain_class, nTest*s,
-                           prob = train_class/nTrain, replace = T)
+  educated_guess <- sample(1:nTrain_class, nTest*s, prob = train_class/nTrain, replace = T)
   educated_guess <- matrix(educated_guess, s, nTest)
 
-  acc_random_guess <- apply(random_guess, 1,
-                            function(xvec){sum(true_class == xvec) / nTest})
-  acc_educated_guess <- apply(educated_guess, 1,
-                              function(xvec){sum(true_class == xvec) / nTest})
-  acc_majority_guess <- sum(
-    true_class == rep(which.max(train_class), nTest)) / nTest
+  acc_random_guess <- apply(random_guess, 1, function(xvec){sum(true_class == xvec) / nTest})
+  acc_educated_guess <- apply(educated_guess, 1, function(xvec){sum(true_class == xvec) / nTest})
+  acc_majority_guess <- sum(true_class == rep(which.max(train_class), nTest)) / nTest
 
   # one-side p value
   p_random_guess <- sum(my_acc <= acc_random_guess)/length(acc_random_guess)
-  p_educated_guess <- sum(
-    my_acc <= acc_educated_guess)/length(acc_educated_guess)
+  p_educated_guess <- sum(my_acc <= acc_educated_guess)/length(acc_educated_guess)
   return(list(my_accuracy=my_acc,
               p_random_guess=p_random_guess,
               p_educated_guess=p_educated_guess,
