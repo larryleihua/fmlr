61eae09b02fb161785c0b65996575e64c0492309
larryleihua <larry.lei.hua@gmail.com>
2020-01-18 16:11:17 -0600

    imp label_meta

--- R/labels.R
+++ R/labels.R
@@ -1,6 +1,6 @@
-#--------------------#
-# LarryHua.com, 2019 #
-#--------------------#
+#-------------------------#
+# Larry Lei Hua 2019-2020 #
+#-------------------------#
 
 #' Meta labeling, including three options: triple barriers, upper and vertical barriers, and lower and vertical barriers.
 #'
@@ -13,7 +13,6 @@
 #'          \item side: 0: both upper and lower barriers; 1: only upper; -1: only lower.
 #'         }
 #' @param ptSl a vector of two multipliers for the upper and lower barriers, respectively.
-#' @param ex_vert whether exclude the output when the vertical barrier is hit; default is T.
 #' @param n_ex number of excluded observations at the begining of x; default is 0.
 #' 
 #' @return data frame with the following columns:
@@ -31,7 +30,7 @@
 #' @author Larry Lei Hua
 #'
 #' @export
-label_meta <- function(x, events, ptSl, ex_vert=T, n_ex=0)
+label_meta <- function(x, events, ptSl, n_ex=0)
 {
   nBar <- length(x)
   t0 <- events$t0
@@ -89,10 +88,5 @@ label_meta <- function(x, events, ptSl, ex_vert=T, n_ex=0)
   out$t1Fea <- t0 - 1
   out$tLabel <- t0 - 1 + apply(out[,c("T_up", "T_lo", "t1")], 1, min)
 
-  if(ex_vert==T)
-  {
-    out <- subset(out, !(is.infinite(T_up) & is.infinite(T_lo)))
-  }
-  
   return(out)
 }
